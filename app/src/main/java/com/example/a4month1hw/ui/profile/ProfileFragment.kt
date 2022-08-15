f5be6ae2758264eed9e0a0a482486a383fb5837f
aidai87 <aidaiataeva87@gmail.com>
2022-08-16 01:37:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/a4month1hw/ui/profile/ProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.example.a4month1hw.ui.profile
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.a4month1hw.R
+import com.example.a4month1hw.databinding.ActivityMainBinding
+import com.example.a4month1hw.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    var launchForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode === AppCompatActivity.RESULT_OK) {
+            val image = result.data?.data
+            binding.image.setImageURI(image)
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.image.setOnClickListener{
+           val intent = Intent(Intent.ACTION_GET_CONTENT)
+           intent.type = "image/*"
+           launchForResult.launch(intent)
+        }
+    }
+}
